0d4dd6a2eb1f95c77cd55f345941250cb58f9c88
Marius Seufzer <marius.seufzer@icloud.com>
2021-03-12 22:21:44 +0100

    lay out basic app flow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
+class Goban { };
+
+// Renderer
+class Renderer {
+public:
+    ~Renderer();
+    virtual void render(const Goban& goban) const = 0;
+};
+
+class TerminalRenderer : public Renderer {
+public:
+    TerminalRenderer() {}
+    ~TerminalRenderer() {}
+
+    void render(const Goban& goban) const override {
+        std::cout << "Printing current Goban";
+    }
+};
+
+// GoGameManager
+class GoGameManager {
+public:
+    GoGameManager() = default;
+    GoGameManager(const Renderer& renderer) :renderer{}, goban{} {}
+
+    bool makeMove(int x, int y) const {
+        // move logic... then:
+        renderer.render(goban);
+        return true;
+    }
+private:
+    Goban goban;
+    Renderer renderer;
+};
+
+// Entry point - main
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const TerminalRenderer &terminalRenderer{};
+    const GoGameManager goGameManager(terminalRenderer);
+
+    goGameManager.makeMove(1,2);
 }
